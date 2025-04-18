5875ad826a9947c198fc8efd0aa5c933921896dd
Harshiv Sing Bhasin <harshiv.bhasin.cse@ghrce.raisoni.net>
2025-04-18 17:08:55 +0530

    3rd_commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/leave-management')
+mongoose.connect('mongodb+srv://hsbhasin03:QLe79OXNu30FrjVv@cluster0.vvui1ab.mongodb.net/leaveApp')
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.log("❌ MongoDB Connection Error:", err));
 
